5188a744f29e559a26ec98383f69ccedb99c5781
Alex N. Rodrigues <65856024+AlexNRodrigues@users.noreply.github.com>
2022-03-07 01:21:53 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,32 +1,63 @@
-var btn = document.querySelector("button.busca-ref");
-btn.addEventListener("click", function () {
-    var div = document.querySelector("#referencia");
-    var filtro1 = document.querySelector("#filtro1");
-    var filtro2 = document.querySelector("#filtro2");
-    var filtro3 = document.querySelector("#filtro3");
+var btn1 = document.querySelector("button.busca-ref1");
+var btn2 = document.querySelector("button.busca-ref2");
+btn1.addEventListener("click", function () {
+  
+  var div = document.querySelector("#referencia");
+  var filtro1 = document.querySelector("#filtro1");
+  var filtro2 = document.querySelector("#filtro2");
+  var filtro3 = document.querySelector("#filtro3");
 
-    if (div.style.display === "none") {
+  if (div.style.display === "none") {
 
-        div.style.display = "block";
-        filtro1.style.display = "none";
-        filtro2.style.display = "none";
-        filtro3.style.display = "none";
+    div.style.display = "block";
+    filtro1.style.display = "none";
+    filtro2.style.display = "none";
+    filtro3.style.display = "none";
 
-        btn.innerHTML = 'Busca por filtro';
+    btn1.innerHTML = 'Busca por filtro';
 
-    } else {
-        div.style.display = "none";
-        filtro1.style.display = "block";
-        filtro2.style.display = "block";
-        filtro3.style.display = "block";
-        btn.innerHTML = 'Busca por referência';
-    }
+  } else {
+    div.style.display = "none";
+    filtro1.style.display = "block";
+    filtro2.style.display = "block";
+    filtro3.style.display = "block";
+    
+    btn1.innerHTML = 'Busca por referência';
+  }
+
+})
+
+btn2.addEventListener("click", function () {
+
+  var divFixed = document.querySelector("#referencia-fixed");
+  var filtroFixed1 = document.querySelector("#filtro-fixed1");
+  var filtroFixed2 = document.querySelector("#filtro-fixed2");
+  var filtroFixed3 = document.querySelector("#filtro-fixed3");
+
+  if (div.style.display === "none") {
+
+    divFixed.style.display = "block";
+    filtroFixed1.style.display = "none";
+    filtroFixed2.style.display = "none";
+    filtroFixed3.style.display = "none";
+
+    btn2.innerHTML = 'Busca por filtro';
+
+  } else {
+    
+    divFixed.style.display = "none";
+    filtroFixed1.style.display = "block";
+    filtroFixed1.style.display = "block";
+    filtroFixed1.style.display = "block";
+    
+    btn2.innerHTML = 'Busca por referência';
+  }
 
 })
 
 document.addEventListener('scroll', rolar);
 
-var ultimaPosicao = 50;
+var ultimaPosicao = 60;
 var telefone = document.querySelector("#telefone");
 var formFixed = document.querySelector("#form-fixed");
 var navbar = document.querySelector(".navbar");
@@ -39,12 +70,12 @@ function rolar() {
         telefone.style.display = "none";
         formFixed.style.display = "block";
         navbar.style.backgroundColor = "#6EADE7";
-//         heroSection.style.display = "none";
+        heroSection.style.display = "none !important";
     } else {
         telefone.style.display = "block";
         formFixed.style.display = "none";
         navbar.style.backgroundColor = "transparent";
-//         heroSection.style.display = "flex";
+        heroSection.style.display = "flex  !important";
     }
 
       ultimaPosicao = atualPosicao;
